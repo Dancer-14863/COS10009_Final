1505a053a6338b6807efb052c755b25ed0223e40
Dancer-14863 <alrider12@gmail.com>
2020-05-27 00:47:18 +0800

    Made some misc changes

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ require 'gosu'
 require 'net/http'
 require "open-uri"
 
+##
 # Contains common file operations
 # used for this project
 module JSONFileOperations
@@ -12,7 +13,6 @@ module JSONFileOperations
   # Clears the contents of a file
   #
   # @param [String] filename Name of the file to be cleared
-  #
   def clearFile(filename)
     # Removes any existing content in the file
     File.open(filename, "w").truncate(0)
@@ -25,7 +25,6 @@ module JSONFileOperations
   #
   # @param [String] filename Name of the file
   # @param [Any]    data     Data to be written
-  #
   def updateFile(filename, data)
     # If the file already exists, its existing contents
     # are cleared
@@ -52,36 +51,49 @@ end
 
 ##
 # TODO: - Add catch statements if the api call fails
-# TODO: - Add private and public methods
 # TODO: - Make sure weather updates if location updates
 #
 class WeatherInformation
   include JSONFileOperations
+
+  ##
+  # Unique value provided by API service
   API_KEY = "2c4294e1b221ea4d08cde0eeba687d31"
+  ##
+  # URL for weather api call
+  # Documentation: https://openweathermap.org/current
   WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?lat=%{lat}&lon=%{lon}&appid=%{apiKey}"
+  ##
+  # Filename for weather information saving
   WEATHER_JSON_FILE_NAME = "forecast.json"
+  ##
+  # URL for ip location api call
+  # Documentation: https://freegeoip.app/
   LOCATION_API_URL = "https://freegeoip.app/json/"
+  ##
+  # File name for location information saving
   LOCATION_JSON_FILE_NAME = "location.json"
+  ##
+  # Time in milliseconds after which location information is considered to be
+  # expired. Set to 10 minutes
   LOCATION_INFORMATION_EXPIRY_TIME_LIMIT = 600
+  ##
+  # Time in milliseconds after which weather information is considered to be
+  # expired. Set to 60 minutes
   WEATHER_INFORMATION_EXPIRY_TIME_LIMIT = 3600
+  ##
+  # URL for the weather icon api call
   WEATHER_ICON_URL = "http://openweathermap.org/img/wn/%{symbolId}@2x.png"
+  ## 
+  # Name for the weather icon file
   WEATHER_ICON_NAME = "weather_symbol.png"
 
   attr_reader :userLocationInfo
 
+  ##
+  # Inits a Weather Information object
   def initialize
     getUserLocation
-  end
-
-  ##
-  # Checks if the location save file
-  # exists and if it does loads its contents
-  # to @userLocationInfo
-  def checkLocationFile
-    if (File.exists?(LOCATION_JSON_FILE_NAME))
-      fileData = readFromFile(LOCATION_JSON_FILE_NAME)
-      @userLocationInfo = JSON.parse(fileData)
-    end
   end
 
   ##
@@ -90,8 +102,7 @@ class WeatherInformation
   #   (1) The user location information is nil
   #   (2) The location information has expired
   #
-  # @returns [Boolean] Returns true if location info should be updated else
-  #                    false
+  # @returns [Boolean] Returns true if location info should be updated else false
   def reupdateUserLocation?
     mustUpdate = true
 
@@ -102,7 +113,6 @@ class WeatherInformation
 
       # Gets the difference between the two datetimes in seconds
       timeDifferenceSeconds = ((dateTimeNow - fetchDateTime) * 24 * 60 * 60).to_i
-
       mustUpdate = timeDifferenceSeconds > LOCATION_INFORMATION_EXPIRY_TIME_LIMIT
     end
 
@@ -130,24 +140,12 @@ class WeatherInformation
   end
 
   ##
-  # Checks if the weather save file
-  # exists and if it does loads its contents
-  # to @userWeatherInfo
-  def checkWeatherFile
-    if (File.exists?(WEATHER_JSON_FILE_NAME))
-      fileData = readFromFile(WEATHER_JSON_FILE_NAME)
-      @userWeatherInfo = JSON.parse(fileData)
-    end
-  end
-
-  ##
   # Checks if the user weather information should be updated
   # This is done if
   #   (1) The user weather information is nil
   #   (2) The weather information has expired
   #
-  # @returns [Boolean] Returns true if weather info should be updated else
-  #                    false
+  # @returns [Boolean] Returns true if weather info should be updated else false
   def reupdateUserWeather?
     mustUpdate = true
 
@@ -201,9 +199,33 @@ class WeatherInformation
     return @userWeatherInfo
   end
 
+  private
+
+  ##
+  # Checks if the location save file
+  # exists and if it does loads its contents
+  # to @userLocationInfo
+  def checkLocationFile
+    if (File.exists?(LOCATION_JSON_FILE_NAME))
+      fileData = readFromFile(LOCATION_JSON_FILE_NAME)
+      @userLocationInfo = JSON.parse(fileData)
+    end
+  end
+
+  ##
+  # Checks if the weather save file
+  # exists and if it does loads its contents
+  # to @userWeatherInfo
+  def checkWeatherFile
+    if (File.exists?(WEATHER_JSON_FILE_NAME))
+      fileData = readFromFile(WEATHER_JSON_FILE_NAME)
+      @userWeatherInfo = JSON.parse(fileData)
+    end
+  end
+
   ##
   # Downloads related weather icon from the
-  # API
+  # API and saves it in a png file
   def downloadWeatherIcon
     unless (@userWeatherInfo.nil?)
       # This is a id attached to the response from the api
@@ -236,10 +258,17 @@ class ForecastApp < Gosu::Window
   BUTTON_WIDTH = 100
   BUTTON_HEIGHT = 50
 
+  ##
+  # Inits the app
   def initialize
     super(WIN_WIDTH, WIN_HEIGHT, false)
     self.caption = "Weather Forecast"
 
+    # Hash containing the colorschemes
+    # Each color sheme has its values stored in 
+    # a pair of 3 element integer array
+    # This first array is used for holding the rgb values for the
+    # background color, the second array for the font color
     @colorShemes = Hash[
       "dark" => [
         [46, 40, 42],
@@ -268,12 +297,15 @@ class ForecastApp < Gosu::Window
     @infoFontLevelOne = Gosu::Font.new(29)
     @infoFontLevelTwo = Gosu::Font.new(23)
 
+    # Fetches location and weather information
     @currentWeatherInfo = WeatherInformation.new
     @weatherForecast = @currentWeatherInfo.getUserWeather
     @userLocation = currentLocationString
     @userWeatherMessage = currentWeatherMessage 
     @weatherIcon = Gosu::Image.new("weather_symbol.png")
 
+    # Array containing the toggle theme
+    # button starting coordinates
     @buttonCoord = [
       WIN_WIDTH - BUTTON_WIDTH - 10,
       10
@@ -286,6 +318,14 @@ class ForecastApp < Gosu::Window
     Gosu.button_down? Gosu::KB_ESCAPE 
     Gosu.button_down? Gosu::MsLeft 
     updateUserDateTime
+
+    # Checks if user location information should be 
+    # updated
+    if (@currentWeatherInfo.reupdateUserLocation?)
+      puts "Updating User Location"
+      @currentWeatherInfo.getUserLocation
+      @userLocation = currentLocationString
+    end
 
     # Checks if the weather information should be updated
     if (@currentWeatherInfo.reupdateUserWeather?)
@@ -300,6 +340,7 @@ class ForecastApp < Gosu::Window
     Gosu.draw_rect(0, 0, WIN_WIDTH, WIN_HEIGHT, @background, ZOrder::BACKGROUND, mode=:default)
     # User Local Date
     @uiFont.draw_text("Date: #{@userDate}",10, 10, ZOrder::MIDDLE, 1.0, 1.0, @primaryFontColor)
+    # Draws theme toggle button outline
     Gosu.draw_rect(
       @buttonCoord[0],
       @buttonCoord[1],
@@ -309,6 +350,7 @@ class ForecastApp < Gosu::Window
       ZOrder::MIDDLE, 
       mode=:default
     )
+    # Draws text for theme toggle button
     @uiFont.draw_text_rel(
       "Toggle",
       (WIN_WIDTH- BUTTON_WIDTH - 10) + BUTTON_WIDTH/ 2,
@@ -320,6 +362,7 @@ class ForecastApp < Gosu::Window
       1.0, 
       @primaryFontColor
     )
+    # Draws user day of the week and local time
     @infoFontLevelTwo.draw_text_rel(
       "#{@userDay}, #{@userTime}", 
       WIN_WIDTH / 2, 
@@ -421,6 +464,10 @@ class ForecastApp < Gosu::Window
   end
 
   ##
+  # Updates the following attributes
+  #   @userTime - current local time in Hour:Minute:Second format
+  #   @userDate - current local date in Year-Month-Date format
+  #   @userDay  - current day of the week
   def updateUserDateTime
     today = DateTime.now
     @userTime = today.strftime("%H:%M:%S")
@@ -456,6 +503,9 @@ class ForecastApp < Gosu::Window
     return (temperature - 273.15).round(1)
   end
 
+  ##
+  # Checks if a button has been pressed
+  # Reused from weekly tasks
   def button_down(id)
     case id
     when Gosu::KB_ESCAPE
@@ -469,6 +519,11 @@ class ForecastApp < Gosu::Window
     end
   end
 
+  ## 
+  # Checks if the mouse cursor is over the theme toggle button
+  # 
+  # @return [Boolean] True/False depending on whether the cursor is over the
+  # button
   def mouse_over_button?(mouse_x, mouse_y)
     if (mouse_x > @buttonCoord[0] && mouse_x < @buttonCoord[0] + BUTTON_WIDTH) && 
         (mouse_y > @buttonCoord[1] && mouse_y < @buttonCoord[1] + BUTTON_HEIGHT)
@@ -478,6 +533,8 @@ class ForecastApp < Gosu::Window
     end
   end
 
+  ## 
+  # Toggles the colorsheme between light/dark
   def toggleTheme
     @currentScheme == "dark" ? @currentScheme = "light" : @currentScheme = "dark"
     @background = Gosu::Color.rgb(
